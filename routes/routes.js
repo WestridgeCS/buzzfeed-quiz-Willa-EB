0b30454c69920d401d0175e0e9985fd4d1fbda47
Willa-EB <WE28320@westridge.org>
2026-01-20 11:25:54 -0800

    I am getting an error when I go to localhost:3000.

--- routes/routes.js
+++ routes/routes.js
@@ -11,12 +11,71 @@ router.get('/', (req, res) => {
   });
 });
 
-// One route handles all choices
 router.post('/quiz', (req, res) => {
+  const body = req.body || {};
+  const state = {
+    emotion: body.emotion || '',
+    priority: body.priority || '',
+    food: body.food || ''
+  };
+
+  let step = 1;
+  if (state.emotion && !state.priority && !state.food) step = 2;
+  if (state.emotion && state.priority && !state.food) step = 3;
+  if (state.emotion && state.priority && state.food) step = 4;
+
+  let outcome = null;
+
+  if (step === 4) {
+    outcome = getOutcome (state)
+  }
   res.render('quiz', {
     title: 'Quiz Question',
-    state: {},
-    outcome: null
+    step,
+    state,
+    outcome
   });
 });
 
+function getOutcome({ emotion, priority, food }) {
+  if (emotion === 'emotionally' && priority === 'fortune' && food=== 'bread') {
+    return {
+      heading: 'Difficult Cat',
+      text: 'You are extremely difficult and picky when it comes to food/'
+    };
+  }
+  else if (emotion === 'content' && priority=== 'family' && food=== 'spaghetti'){
+    return {
+      heading: 'Fluffy Kitten',
+      text: 'Objectively the cutest and the most innocent.'
+    }
+  }
+  else if (emotion === 'emotionally' && priority==='love' && food=== 'bread')  {
+    return {
+      heading: 'Mid-life Crisis Cat',
+      text: 'I hope life gets easier for you soon.'
+    }
+  }
+  else if (emotion=== 'content' && priority=== 'fame' && food=== 'pancakes'){
+    return {
+      heading: 'Flying Cat',
+      text: 'Keep living life to the fullest!'
+    }
+  }
+  else if (emotion=== 'hungry' && priority=== 'fortune' && food=== 'fillet'){
+    return {
+      heading: 'Hungry Cat',
+      text: 'Get some chow!'
+    }
+  }
+  else if (emotion=== 'content' && priority=== 'fortune' && food=== 'fillet') {
+    return {
+      heading: 'Loaded Cat',
+      text: 'Start kneading that dough!'
+    }
+  }
+  return {
+    heading: 'Boring',
+    text: 'Get a personality and try again!'
+  };
+}
